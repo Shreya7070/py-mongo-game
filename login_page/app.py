d20b471bba83558c2d13f6a7f3dd8eacd7b6b81a
Aditya Pai <adityaajitpai@gmail.com>
2022-12-02 00:45:57 +0530

    added login

--- login_page/app.py
+++ login_page/app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 # Database
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb', 27017)
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017/', 27017)
 db = client.user_login_system
-
 # Decorators
 def login_required(f):
   @wraps(f)
@@ -31,7 +30,3 @@ def home():
 @login_required
 def dashboard():
   return render_template('dashboard.html')
-
-         
-if __name__ == '__main__':
-    app.run(port = 6789, debug = True)
